fc0d141fcf7b83d125d79f0619be1551130186c8
Quoc Le <pociosx@gmail.com>
2018-09-12 22:34:39 +0700

    init

---
+++ Example/Pods/Target Support Files/PocUtil/PocUtil-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PocUtil : NSObject
+@end
+@implementation PodsDummy_PocUtil
+@end
---
+++ Example/Pods/Target Support Files/Pods-PocUtil_Tests/Pods-PocUtil_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PocUtil_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_PocUtil_Tests
+@end
